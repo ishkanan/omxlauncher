90ce4aa9e5b217e1d70ddceb9895f0581bcbb5b1
Anthony Ishkan <anthony.ishkan@gmail.com>
2018-12-06 19:02:33 +1100

    Fix script. Add requirements.

--- omxlauncher.py
+++ omxlauncher.py
@@ -1,56 +1,73 @@
 #!/usr/bin/python
+
 import logging
-import pexpect
-import time
 import os
+import pexpect
+import signal
+import sys
+import time
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
+OMX_CMD = "omxplayer -adev hdmi --threshold 1 {url}"
+#OMX_CMD = "google-chrome {url}"
+child = None
+logger = None
 
-#stream datasources
+def signal_handler(signum, frame):
+    if child == None:
+        logger.debug("omxplayer not launched, nothing to do.")
+    else:
+        logger.info("Force-closing omxplayer...")
+        child.close(force=True)
+    logger.info("Gracefully stopped. Bye.")
+    sys.exit(0)
 
-#ZAC TEST GP2018
-STAGE = 'rtsp://wowza.mmf:1935/live/foh.stream'
+def make_logger(name, out_file):
+    """Creates a logger with specified name that writes to console and
+    specified output file.
+    """
+    l = logging.getLogger(name)
+    l.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s  - %(levelname)s - %(message)s')
+    ch = logging.FileHandler(out_file)
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(formatter)
+    l.addHandler(ch)
+    ch = logging.StreamHandler(stream=sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(formatter)
+    l.addHandler(ch)
+    return l
 
-# create logger
-logger = logging.getLogger('simple_example')
-logger.setLevel(logging.DEBUG)
+def main(args):
+    """Runs a loop to ensure omxplayer is always running, and keeps retrying
+    in the event of a disconnect.
+    """
+    global child
+    cmd_to_run = OMX_CMD.format(url=args[0])
 
-# create console handler and set level to debug
-ch = logging.FileHandler('omxlauncher.log')
-ch.setLevel(logging.DEBUG)
+    logger.debug("Entering loop to keep omxplayer running...")
+    while True:
+        logger.debug("Launching omxplayer...")
+        child = pexpect.spawn(cmd_to_run)
+        ret = child.expect([pexpect.TIMEOUT, pexpect.EOF], timeout=10)
+        if ret == 0:
+            logger.debug("omxplayer is running. Yay.")
+            #child.send("2")               # send 2 to exploit bug that syncs stream   
+            #child.interact()              # Give control of the child to the user.
+        else:
+            logger.debug("omxplayer failed to start, retrying in 10 seconds...")
+            time.sleep(10)
+            continue
 
-# create formatter
-formatter = logging.Formatter('%(asctime)s  - %(levelname)s - %(message)s')
+        ret = child.expect([pexpect.TIMEOUT, pexpect.EOF], timeout=5)
+        while ret == 0:
+            ret = child.expect([pexpect.TIMEOUT, pexpect.EOF], timeout=5)
+        logger.debug("omxplayer has exited, will restart...")
 
-# add formatter to ch
-ch.setFormatter(formatter)
-
-# add ch to logger
-logger.addHandler(ch)
-
-
-#logging.basicConfig(filename='omxlauncher.log',level=logging.DEBUG)
-logger.debug('launcher boot')
-while (1 > 0):
-        #build command
-        omx_cmd = "omxplayer -o hdmi {} ".format(STAGE)
-        running = 0
-        #omx_cmd = "omxplayer  {} ".format(STAGE)
-        child = pexpect.spawn (omx_cmd)
-        a = child.expect([pexpect.TIMEOUT,pexpect.EOF], timeout=10) # wait 10 sec for process to end
-        #logging.debug(a.before)
-        if (a ==0):
-                logger.debug('player start')
-                time.sleep(10)
-                child.send('2')               # send 2 to exploit bug that syncs stream   
-                #child.interact()              # Give control of the child to the user.
-                #logging.debug('launcher running')
-                a=0
-                running = 1
-        while (a == 0):
-                a = child.expect([pexpect.TIMEOUT,pexpect.EOF], timeout=300) # wait 5min for process to end
-                if (a ==0):
-                        running = 1
-
-        if (running == 1):
-                logger.debug('player exit')
+if __name__ == "__main__":    
+    if len(sys.argv) < 3:
+        print("USAGE: omxlauncher <log file> <feed URL>")
+        sys.exit(-1)
+    logger = make_logger("omxlauncher", sys.argv[1])
+    signal.signal(signal.SIGINT, signal_handler)
+    main(sys.argv[2:])
