d2b842d9d91a8787faab5f22a668baea6398754a
Anthony Ishkan <anthony.ishkan@gmail.com>
2019-03-08 23:15:03 +1100

    Patch for apt

--- omxlauncher.sh
+++ omxlauncher.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# die apt die!
+systemctl stop apt-daily.timer
+systemctl disable apt-daily.timer
+systemctl stop apt-daily.service
+systemctl disable apt-daily.service
+systemctl kill --kill-who=all apt-daily.service
+while ! (systemctl list-units --all apt-daily.service | fgrep -q dead)
+do
+  sleep 1;
+done
+
+# go go
 cd /home/pi/omxlauncher
 apt-get install -y python3-venv
 python3 -m venv .
@@ -10,3 +22,4 @@ pip install -r requirements.txt
 python omxlauncher.py "/home/pi/omxlauncher/omxlauncher.log" "$1"
 deactivate
 cd /home/pi/omxlauncher
+
